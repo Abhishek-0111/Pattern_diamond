db812c41f59ed47929fe4e4ddf46ea15d5022594
Abhishek-0111 <43722870+Abhishek-0111@users.noreply.github.com>
2018-10-10 22:51:56 +0530

    Create dia.py

---
+++ dia.py
@@ -0,0 +1,14 @@
+n = int(input('dimen ?\n'))
+tmp = n
+c = 0
+while tmp:
+    c += 1
+    tmp //= 10
+for i in range(n):
+    print()
+    count = 1
+    for j in range((n//2)+1):
+        if j <= i < (n // 2 + 1):
+            print("{0:{1}d}".format(count, c), end=' ')
+            count += 1
+    
